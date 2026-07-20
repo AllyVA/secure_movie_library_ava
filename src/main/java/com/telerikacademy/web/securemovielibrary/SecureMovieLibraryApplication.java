f148810ee87cfbb475e02a8c9e6bd0697999f7de
AllyVA <alexandraalexova12@gmail.com>
2026-07-20 22:06:37 +0300

    spring

---
+++ src/main/java/com/telerikacademy/web/securemovielibrary/SecureMovieLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.telerikacademy.web.securemovielibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureMovieLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureMovieLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/telerikacademy/web/securemovielibrary/SecureMovieLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telerikacademy.web.securemovielibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureMovieLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
