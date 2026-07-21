163bd67d3ecc734b9c0e33f46023f0eae24d6038
AllyVA <alexandraalexova12@gmail.com>
2026-07-21 12:31:55 +0300

    Configure database and add initial movie data

--- db/create.sql
+++ db/create.sql
@@ -1,3 +1,13 @@
 CREATE DATABASE IF NOT EXISTS secure_movie_library;
 
 USE secure_movie_library;
+
+create table if not exists movies
+(
+    movie_id           int auto_increment
+        primary key,
+    movie_title        varchar(200)  not null,
+    movie_director     varchar(150)  null,
+    movie_release_year int           null,
+    movie_rating       decimal(3, 1) null
+);
---
+++ db/insert-data.sql
@@ -0,0 +1,16 @@
+use secure_movie_library;
+
+insert into movies
+    (movie_title, movie_director, movie_release_year, movie_rating)
+VALUES
+    ('The Great Gatsby', 'Baz Luhrmann', 2013, 7.2),
+    ('Grease', 'Randal Kleiser', 1978, 7.2),
+    ('Armageddon', 'Michael Bay', 1998, 6.7),
+    ('The Matrix', 'Lana Wachowski, Lilly Wachowski', 1999, 8.7),
+    ('Spirited Away', 'Hayao Miyazaki', 2001, 8.6),
+    ('My Neighbour Totoro', 'Hayao Miyazaki', 1988, 8.1),
+    ('The Fast and the Furious', 'Rob Cohen', 2001, 6.8),
+    ('Harry Potter and the Philosopher''s Stone', 'Chris Columbus', 2001, 7.7),
+    ('The Chronicles of Narnia: The Lion, the Witch and the Wardrobe',
+     'Andrew Adamson', 2005, 6.9),
+    ('Mulan', 'Tony Bancroft, Barry Cook', 1998, 7.7);
